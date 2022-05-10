76ffb2bd43412a3ca6565e6fc82f5b2497139cd3
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2022-05-10 14:38:31 +1000

    Added shopping-list form ( template-drive approach)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppinglistService } from '../shoppinglist.service';
+import { ShoppingListService } from '../shoppinglist.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,20 +9,21 @@ import { ShoppinglistService } from '../shoppinglist.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef; //pass name of local reference #nameInput through ViewChild
-  @ViewChild('amountInput') amountInputRef: ElementRef; //pass name of local reference #namountInput through ViewChild
+  // @ViewChild('nameInput') nameInputRef: ElementRef; //pass name of local reference #nameInput through ViewChild
+  // @ViewChild('amountInput') amountInputRef: ElementRef; //pass name of local reference #namountInput through ViewChild
  // @Output() ingredientAdded = new EventEmitter<Ingredient>(); //pass Ingredient data through event
   // Other option: IngredientAdded = new EventEmitter<{name: string, amount: number}>();
 
-  constructor(private shoppinglistService: ShoppinglistService) { }
+  constructor(private shoppinglistService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem () {
-    const ingName = this.nameInputRef.nativeElement.value; //create const for name using inout value
-    const ingAmount = this.amountInputRef.nativeElement.value; //create const for amount using inout value
-    const newIngredient = new Ingredient(ingName, ingAmount) //pass constant data to create new ingredient
+  onAddItem (form: NgForm) {
+    const value = form.value;
+    // const ingName = this.nameInputRef.nativeElement.value; //create const for name using inout value
+    // const ingAmount = this.amountInputRef.nativeElement.value; //create const for amount using inout value
+    const newIngredient = new Ingredient(value.name, value.amount); //pass constant data to create new ingredient
     //this.ingredientAdded.emit(newIngredient); //emit custom event and pass new ingredient as data
     this.shoppinglistService.addIngredient(newIngredient);
   }
